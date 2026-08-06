334350532dc1cc15e13a138e02d024c1dff57d36
illicchpv <illich58@mail.ru>
2026-08-06 11:10:13 +0300

    + Урок 3. Получение данных с сервера

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,10 +1,7 @@
 const elements = () => {
   const elements = document.querySelectorAll('.set-bg');
 
-  elements.forEach((element) => {
-    const bg = element.getAttribute('data-setbg');
-    element.style.backgroundImage = `url(${bg})`;
-  });
+  elements.forEach((element) => element.style.backgroundImage = `url(${element.dataset.setbg})`);
 };
 
 elements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,5 +1,4 @@
 const mainData = () => {
-  console.log('mainData');
 
   fetch('https://myproject-4cdbd-default-rtdb.firebaseio.com/db-anime.json') // fetch('db-anime.json')
     .then(response => {
@@ -9,12 +8,41 @@ const mainData = () => {
       return response.json();
     })
     .then(data => {
-      console.log('data.anime[0]: ', JSON.stringify(data.anime[0], null, 2));
-      return data;
+      const ganres = new Set();
+      data.forEach(anime => {
+        ganres.add(anime.ganre);
+      });
+      renderAnimeList(data, ganres);
+
+      const sorted5 = data.sort((a, b) => b.views - a.views).slice(0, 5);
+      renderTopAnime(sorted5);
     })
     .catch(error => {
       console.error('Error:', error);
     });
+
+  function renderAnimeList(animeList, ganres) {
+    console.log('renderAnimeList animeList: ', animeList);
+    console.log('renderAnimeList ganres: ', ganres);
+  }
+
+  function renderTopAnime(animeList) {
+    const wrapper = document.querySelector('.filter__gallery');
+    wrapper.innerHTML = '';
+
+    animeList.forEach(anime => {
+      wrapper.insertAdjacentHTML('beforeend', `
+<div class="product__sidebar__view__item set-bg mix" 
+  data-setbg="${anime.image}" 
+>
+  <div class="ep">${anime.rating} / 10</div>
+  <div class="view"><i class="fa fa-eye"></i> ${anime.views}</div>
+  <h5><a href="/anime-details.html">${anime.title}</a></h5>
+</div>
+`);
+    });
+    wrapper.querySelectorAll('.set-bg').forEach((element) => element.style.backgroundImage = `url(${element.dataset.setbg})`);
+  };
 };
 
 mainData();
